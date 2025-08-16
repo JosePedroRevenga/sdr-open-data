681d5ab3786a4a9c494dd7e77281f5565ffbefe4
JPRevA <jp_210692@hotmail.com>
2025-08-16 19:19:19 +0200

    Base Maven project created

---
+++ transportHub/src/test/java/com/alipe/transportHub/TransportHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipe.transportHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
